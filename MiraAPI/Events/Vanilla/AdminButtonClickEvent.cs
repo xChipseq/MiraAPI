3ea46d5574e87c10b0f271d5d4d4fa790341ac51
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-12-08 14:44:01 -0500

    more events

---
+++ MiraAPI/Events/Vanilla/AdminButtonClickEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace MiraAPI.Events.Vanilla;
+
+/// <summary>
+/// Button click event for <see cref="AdminButton"/> from Vanilla Among Us.
+/// </summary>
+public class AdminButtonClickEvent : MiraCancelableEvent
+{
+    /// <summary>
+    /// Gets the instance of <see cref="AdminButton"/> that was clicked.
+    /// </summary>
+    public AdminButton Button { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdminButtonClickEvent"/> class.
+    /// </summary>
+    /// <param name="button">The AdminButton.</param>
+    public AdminButtonClickEvent(AdminButton button)
+    {
+        Button = button;
+    }
+}
---
+++ MiraAPI/Events/Vanilla/ReportButtonClickEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace MiraAPI.Events.Vanilla;
+
+/// <summary>
+/// Button click event for <see cref="ReportButton"/> from Vanilla Among Us.
+/// </summary>
+public class ReportButtonClickEvent : MiraCancelableEvent
+{
+    /// <summary>
+    /// Gets the instance of <see cref="ReportButton"/> that was clicked.
+    /// </summary>
+    public ReportButton Button { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReportButtonClickEvent"/> class.
+    /// </summary>
+    /// <param name="button">The ReportButton.</param>
+    public ReportButtonClickEvent(ReportButton button)
+    {
+        Button = button;
+    }
+}
