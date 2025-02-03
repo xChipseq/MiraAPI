8e2535ada25339845470623541afe08386bfc326
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-02-02 20:42:23 -0500

    yes it works

---
+++ MiraAPI.Example/Buttons/TestButton.cs
@@ -0,0 +1,27 @@
+﻿using MiraAPI.Example.Modifiers;
+using MiraAPI.Hud;
+using MiraAPI.Modifiers;
+using MiraAPI.Utilities.Assets;
+using Reactor.Utilities.Extensions;
+using UnityEngine;
+
+namespace MiraAPI.Example.Buttons;
+
+[RegisterButton]
+public class TestButton : CustomActionButton
+{
+    public override string Name => "Test Button";
+    public override float Cooldown => 0f;
+    public override LoadableAsset<Sprite> Sprite => ExampleAssets.ExampleButton;
+
+    protected override void OnClick()
+    {
+        var randomPlayer = PlayerControl.AllPlayerControls.ToArray().Random();
+        PlayerControl.LocalPlayer.RpcAddModifier<ModifierParams>("test", 1, randomPlayer);
+    }
+
+    public override bool Enabled(RoleBehaviour? role)
+    {
+        return true;
+    }
+}
---
+++ MiraAPI.Example/Modifiers/ModifierParams.cs
@@ -0,0 +1,16 @@
+﻿using MiraAPI.Modifiers;
+using Reactor.Utilities;
+
+namespace MiraAPI.Example.Modifiers;
+
+[RegisterModifier]
+public class ModifierParams : BaseModifier
+{
+    public override string ModifierName => "ModifierParams";
+
+    public ModifierParams(string param1, int param2, PlayerControl playerControl)
+    {
+        Logger<ExamplePlugin>.Error($"Param1: {param1}, Param2: {param2}");
+        Logger<ExamplePlugin>.Error($"Player: {playerControl.PlayerId}");
+    }
+}
