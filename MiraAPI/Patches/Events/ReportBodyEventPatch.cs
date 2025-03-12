a04e0d28dbb7e85412ced52ea029fef3cc272e92
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-03-12 17:24:40 -0400

    WTF GIT FAIL!

--- MiraAPI/Patches/Events/ReportBodyEventPatch.cs
+++ MiraAPI/Patches/Events/ReportBodyEventPatch.cs
@@ -1,26 +1,21 @@
 ﻿using HarmonyLib;
 using MiraAPI.Events;
 using MiraAPI.Events.Vanilla.Meeting;
+using MiraAPI.Utilities;
 
 namespace MiraAPI.Patches.Events;
 
-<<<<<<<< HEAD:MiraAPI/Patches/Events/ReportBodyEventPatch.cs
 [HarmonyPatch]
 public static class ReportBodyEventPatch
-========
-[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Start))]
-public static class StartMeetingEventPatch
->>>>>>>> dev:MiraAPI/Patches/Events/StartMeetingEventPatch.cs
 {
-    public static void Postfix(MeetingHud __instance)
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CmdReportDeadBody))]
+    public static bool PlayerControlCmdReportDeadBodyPrefix(PlayerControl __instance, NetworkedPlayerInfo? target)
     {
-<<<<<<<< HEAD:MiraAPI/Patches/Events/ReportBodyEventPatch.cs
         var body = target != null ? Helpers.GetBodyById(target.PlayerId) : null;
 
         var @event = new ReportBodyEvent(__instance, target, body);
-========
-        var @event = new StartMeetingEvent(__instance);
->>>>>>>> dev:MiraAPI/Patches/Events/StartMeetingEventPatch.cs
         MiraEventManager.InvokeEvent(@event);
+        return !@event.IsCancelled;
     }
 }
