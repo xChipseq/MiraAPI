75a15dc090c17a6fec866fda5a931c5c92c1f9fc
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-03-25 17:09:16 -0400

    fix minigame patch

---
+++ MiraAPI/Patches/MinigameBeginErrorPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace MiraAPI.Patches;
+
+[HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
+internal static class MinigameBeginErrorPatch
+{
+    public static void Prefix(Minigame __instance)
+    {
+        __instance.logger ??= new Logger(Logger.Category.Gameplay, "Minigame");
+    }
+}
