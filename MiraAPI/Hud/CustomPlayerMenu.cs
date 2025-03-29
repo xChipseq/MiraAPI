b28a0a125374c6944324c970fe7367f42d4c1099
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-03-29 13:35:16 -0400

    cleanup CustomPlayerMenu.cs

--- MiraAPI/Hud/CustomPlayerMenu.cs
+++ MiraAPI/Hud/CustomPlayerMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using AmongUs.GameOptions;
 using Il2CppInterop.Runtime.Attributes;
@@ -8,6 +9,8 @@ using Reactor.Utilities.Attributes;
 using Reactor.Utilities.Extensions;
 using UnityEngine;
 using UnityEngine.Events;
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
 namespace MiraAPI.Hud;
 
@@ -16,22 +19,18 @@ namespace MiraAPI.Hud;
 /// </summary>
 /// <param name="il2CppPtr">Used by Il2Cpp. Do not use constructor, this is a MonoBehaviour.</param>
 [RegisterInIl2Cpp]
+[SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "Unity Convention")]
+[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:Accessible fields should begin with upper-case letter", Justification = "Unity Convention")]
+[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "Unity Convention")]
 public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
 {
-    public ShapeshifterPanel PanelPrefab;
-
-    public float XStart = -0.8f;
-
-    public float YStart = 2.15f;
-
-    public float XOffset = 1.95f;
-
-    public float YOffset = -0.65f;
-
-    public UiElement BackButton;
-
-    public UiElement DefaultButtonSelected;
-
+    public ShapeshifterPanel panelPrefab;
+    public float xStart = -0.8f;
+    public float yStart = 2.15f;
+    public float xOffset = 1.95f;
+    public float yOffset = -0.65f;
+    public UiElement backButton;
+    public UiElement defaultButtonSelected;
     public List<ShapeshifterPanel> potentialVictims;
 
     /// <summary>
@@ -42,17 +41,17 @@ public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
     {
         var shapeShifterRole = RoleManager.Instance.GetRole(RoleTypes.Shapeshifter);
 
-        var ogMenu = shapeShifterRole.TryCast<ShapeshifterRole>().ShapeshifterMenu;
+        var ogMenu = shapeShifterRole.TryCast<ShapeshifterRole>()!.ShapeshifterMenu;
         var newMenu = Instantiate(ogMenu);
         var customMenu = newMenu.gameObject.AddComponent<CustomPlayerMenu>();
 
-        customMenu.PanelPrefab = newMenu.PanelPrefab;
-        customMenu.XStart = newMenu.XStart;
-        customMenu.YStart = newMenu.YStart;
-        customMenu.XOffset = newMenu.XOffset;
-        customMenu.YOffset = newMenu.YOffset;
-        customMenu.BackButton = newMenu.BackButton;
-        var back = customMenu.BackButton.GetComponent<PassiveButton>();
+        customMenu.panelPrefab = newMenu.PanelPrefab;
+        customMenu.xStart = newMenu.XStart;
+        customMenu.yStart = newMenu.YStart;
+        customMenu.xOffset = newMenu.XOffset;
+        customMenu.yOffset = newMenu.YOffset;
+        customMenu.backButton = newMenu.BackButton;
+        var back = customMenu.backButton.GetComponent<PassiveButton>();
         back.OnClick.RemoveAllListeners();
         back.OnClick.AddListener((UnityAction)(() =>
         {
@@ -65,7 +64,7 @@ public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
 
         newMenu.DestroyImmediate();
 
-        customMenu.transform.SetParent(Camera.main.transform, false);
+        customMenu.transform.SetParent(Camera.main!.transform, false);
         customMenu.transform.localPosition = new Vector3(0f, 0f, -50f);
         return customMenu;
     }
@@ -75,6 +74,7 @@ public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
         ControllerManager.Instance.CloseOverlayMenu(name);
     }
 
+    /// <inheritdoc />
     public override void Begin(PlayerTask task)
     {
         throw new NotImplementedException("Use the other Begin method.");
@@ -90,7 +90,7 @@ public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
     {
         MinigameStubs.Begin(this, null);
 
-        var back = BackButton.GetComponent<PassiveButton>();
+        var back = backButton.GetComponent<PassiveButton>();
         back.OnClick.AddListener((UnityAction)(() =>
         {
             onClick(null);
@@ -107,13 +107,13 @@ public class CustomPlayerMenu(IntPtr il2CppPtr) : Minigame(il2CppPtr)
             var num = i % 3;
             var num2 = i / 3;
             var flag = PlayerControl.LocalPlayer.Data.Role.NameColor == player.Data.Role.NameColor;
-            var shapeshifterPanel = Instantiate(PanelPrefab, transform);
-            shapeshifterPanel.transform.localPosition = new Vector3(XStart + num * XOffset, YStart + num2 * YOffset, -1f);
+            var shapeshifterPanel = Instantiate(panelPrefab, transform);
+            shapeshifterPanel.transform.localPosition = new Vector3(xStart + num * xOffset, yStart + num2 * yOffset, -1f);
             shapeshifterPanel.SetPlayer(i, player.Data, (Il2CppSystem.Action)(() => { onClick(player); }));
             shapeshifterPanel.NameText.color = flag ? player.Data.Role.NameColor : Color.white;
             potentialVictims.Add(shapeshifterPanel);
             list2.Add(shapeshifterPanel.Button);
         }
-        ControllerManager.Instance.OpenOverlayMenu(name, BackButton, DefaultButtonSelected, list2);
+        ControllerManager.Instance.OpenOverlayMenu(name, backButton, defaultButtonSelected, list2);
     }
 }
