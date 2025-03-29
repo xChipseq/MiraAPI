b99efaa420442fc756b176c373e2edcefb10ad46
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-03-29 14:25:57 -0400

    create loadable asset wrapper

---
+++ MiraAPI/Utilities/Assets/LoadableAssetWrapper.cs
@@ -0,0 +1,25 @@
+﻿using Reactor.Utilities.Extensions;
+
+namespace MiraAPI.Utilities.Assets;
+
+/// <summary>
+/// Wrapper class to turn any UnityEngine.Object into a <see cref="LoadableAsset{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of the asset to be loaded. Must be a subclass of <see cref="UnityEngine.Object"/>.</typeparam>
+public class LoadableAssetWrapper<T> : LoadableAsset<T> where T : UnityEngine.Object
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoadableAssetWrapper{T}"/> class.
+    /// </summary>
+    /// <param name="asset">The asset to wrap. This should be a reference to a UnityEngine.Object.</param>
+    public LoadableAssetWrapper(T asset)
+    {
+        LoadedAsset = asset.DontDestroy();
+    }
+
+    /// <inheritdoc />
+    public override T LoadAsset()
+    {
+        return LoadedAsset ?? throw new System.InvalidOperationException("The asset could not be loaded!");
+    }
+}
