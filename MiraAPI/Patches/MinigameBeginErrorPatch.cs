7a7204b0e0f12e1228f25361adc2ce19136d97ee
ang-xd <angxdxdd@gmail.com>
2025-04-22 18:14:45 -0400

    Fix minigame crashes

--- MiraAPI/Patches/MinigameBeginErrorPatch.cs
+++ MiraAPI/Patches/MinigameBeginErrorPatch.cs
@@ -2,11 +2,18 @@
 
 namespace MiraAPI.Patches;
 
-[HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
-internal static class MinigameBeginErrorPatch
+/// <summary>
+/// Used to fix the Minigame crash errors (credits Submerged/PH Gaming for the fix)
+/// </summary>
+[HarmonyPatch]
+public static class MinigameBeginErrorPatch
 {
+    private static readonly Logger _logger = new(Logger.Category.Gameplay, "Minigame");
+
+    [HarmonyPatch(typeof(Minigame), nameof(Minigame.Begin))]
+    [HarmonyPrefix]
     public static void Prefix(Minigame __instance)
     {
-        __instance.logger ??= new Logger(Logger.Category.Gameplay, "Minigame");
+        __instance.logger ??= _logger;
     }
 }
