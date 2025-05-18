9180dec9e15709e72df1dd9d781865b14ef584e9
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-05-17 23:16:01 -0400

    update example mod to fit new role update

--- MiraAPI.Example/Roles/ChameleonRole.cs
+++ MiraAPI.Example/Roles/ChameleonRole.cs
@@ -26,30 +26,30 @@ public class ChameloenRole : CrewmateRole, ICustomRole
         Logger<ExamplePlugin>.Error("Initializing ChamelonRole for player: " + player.PlayerId);
     }
 
-    public void PlayerControlFixedUpdate(PlayerControl playerControl)
+    public void FixedUpdate()
     {
-        if (playerControl.MyPhysics.Velocity.magnitude > 0)
+        if (Player.MyPhysics.Velocity.magnitude > 0)
         {
-            SpriteRenderer rend = playerControl.cosmetics.currentBodySprite.BodySprite;
-            TextMeshPro tmp = playerControl.cosmetics.nameText;
+            var rend = Player.cosmetics.currentBodySprite.BodySprite;
+            var tmp = Player.cosmetics.nameText;
             tmp.color = Color.Lerp(tmp.color, new Color(tmp.color.r, tmp.color.g, tmp.color.b, 1), Time.deltaTime * 4f);
             rend.color = Color.Lerp(rend.color, new Color(1, 1, 1, 1), Time.deltaTime * 4f);
 
-            foreach (var cosmetic in playerControl.cosmetics.transform.GetComponentsInChildren<SpriteRenderer>())
+            foreach (var cosmetic in Player.cosmetics.transform.GetComponentsInChildren<SpriteRenderer>())
             {
                 cosmetic.color = Color.Lerp(cosmetic.color, new Color(1, 1, 1, 1), Time.deltaTime * 4f);
             }
         }
         else
         {
-            SpriteRenderer rend = playerControl.cosmetics.currentBodySprite.BodySprite;
-            TextMeshPro tmp = playerControl.cosmetics.nameText;
-            tmp.color = Color.Lerp(tmp.color, new Color(tmp.color.r, tmp.color.g, tmp.color.b, playerControl.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
-            rend.color = Color.Lerp(rend.color, new Color(1, 1, 1, playerControl.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
+            SpriteRenderer rend = Player.cosmetics.currentBodySprite.BodySprite;
+            TextMeshPro tmp = Player.cosmetics.nameText;
+            tmp.color = Color.Lerp(tmp.color, new Color(tmp.color.r, tmp.color.g, tmp.color.b, Player.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
+            rend.color = Color.Lerp(rend.color, new Color(1, 1, 1, Player.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
 
-            foreach (var cosmetic in playerControl.cosmetics.transform.GetComponentsInChildren<SpriteRenderer>())
+            foreach (var cosmetic in Player.cosmetics.transform.GetComponentsInChildren<SpriteRenderer>())
             {
-                cosmetic.color = Color.Lerp(cosmetic.color, new Color(1, 1, 1, playerControl.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
+                cosmetic.color = Color.Lerp(cosmetic.color, new Color(1, 1, 1, Player.AmOwner ? 0.3f : 0), Time.deltaTime * 4f);
             }
         }
     }
