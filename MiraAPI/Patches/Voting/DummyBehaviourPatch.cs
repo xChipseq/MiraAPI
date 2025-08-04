e7fba386aad787d781b0fea40144539d4b47d88e
xChipseq <sztabekminecraft@gmail.com>
2025-08-04 20:53:50 +0200

    dummy changes

--- MiraAPI/Patches/Voting/DummyBehaviourPatch.cs
+++ MiraAPI/Patches/Voting/DummyBehaviourPatch.cs
@@ -1,16 +1,71 @@
-﻿using HarmonyLib;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Meeting.Voting;
 using MiraAPI.Utilities;
+using MiraAPI.Voting;
+using Reactor.Utilities;
+using Reactor.Utilities.Extensions;
+using UnityEngine;
 
 namespace MiraAPI.Patches.Voting;
 
-[HarmonyPatch(typeof(DummyBehaviour), nameof(DummyBehaviour.Update))]
-internal static class DummyBehaviourPatch
+[HarmonyPatch(typeof(DummyBehaviour))]
+internal static class DummyBehaviourPatches
 {
-    public static void Postfix(DummyBehaviour __instance)
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(DummyBehaviour.Update))]
+    public static bool DummyUpdatePatch(DummyBehaviour __instance)
     {
-        if (MeetingHud.Instance && __instance.myPlayer.GetVoteData())
+        NetworkedPlayerInfo data = __instance.myPlayer.Data;
+        if (data == null || data.IsDead) return false;
+
+        if (MeetingHud.Instance && !__instance.DidVote())
         {
-            __instance.voted = __instance.myPlayer.GetVoteData().VotesRemaining == 0;
+            Coroutines.Start(CoDoVote(__instance));
         }
+
+        return false;
+    }
+
+    private static IEnumerator CoDoVote(DummyBehaviour dummy)
+    {
+        dummy.voted = true;
+        yield return new WaitForSeconds(dummy.voteTime.Next());
+        if (DidVote(dummy)) yield break;
+
+        var dummyVoteEvent = new DummyVoteEvent(dummy);
+        MiraEventManager.InvokeEvent(dummyVoteEvent);
+
+        if (dummyVoteEvent.IsCancelled)
+        {
+            dummy.voted = false;
+            yield break;
+        }
+
+        List<byte> potentialSuspects = new();
+        potentialSuspects.AddRange(PlayerControl.AllPlayerControls
+            .ToArray()
+            .Where(p => p != dummy.myPlayer)
+            .Where(p => dummyVoteEvent.PlayerIsValid(p))
+            .Select(p => p.PlayerId));
+
+        if (dummyVoteEvent.CanSkip)
+        {
+            potentialSuspects.Add(253);
+        }
+
+        if (dummy.myPlayer.GetVoteData().VotesRemaining > 0)
+        {
+            VotingUtils.RpcCastVote(PlayerControl.LocalPlayer, dummy.myPlayer.PlayerId, potentialSuspects.Random());
+        }
+    }
+
+    private static bool DidVote(this DummyBehaviour dummy)
+    {
+        var dummyVoteData = dummy.myPlayer.GetVoteData();
+        return dummyVoteData.Votes.Count > 0;
     }
 }
